5189838dda850293c3aad690ed3d6d657adbb2ae
n8n-karll <n8n-karll@users.noreply.github.com>
2026-06-30 04:16:13 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict sKvEdDwCHAs7WxHSu8dc8ItCk5lf0FUjny0YceEzaK6zMg5QoyBxwewwGmBgndA
+-- \restrict FDhtaOhtkA6bhhGBnzBSdCQs0cAhOfbg2vUarX7z2ytqNEIZ1rEd0tbH9HzYTeL
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -33,7 +33,7 @@ COPY "auth"."audit_log_entries" ("instance_id", "id", "payload", "created_at", "
 -- Data for Name: custom_oauth_providers; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
-COPY "auth"."custom_oauth_providers" ("id", "provider_type", "identifier", "name", "client_id", "client_secret", "acceptable_client_ids", "scopes", "pkce_enabled", "attribute_mapping", "authorization_params", "enabled", "email_optional", "issuer", "discovery_url", "skip_nonce_check", "cached_discovery", "discovery_cached_at", "authorization_url", "token_url", "userinfo_url", "jwks_uri", "created_at", "updated_at") FROM stdin;
+COPY "auth"."custom_oauth_providers" ("id", "provider_type", "identifier", "name", "client_id", "client_secret", "acceptable_client_ids", "scopes", "pkce_enabled", "attribute_mapping", "authorization_params", "enabled", "email_optional", "issuer", "discovery_url", "skip_nonce_check", "cached_discovery", "discovery_cached_at", "authorization_url", "token_url", "userinfo_url", "jwks_uri", "created_at", "updated_at", "custom_claims_allowlist") FROM stdin;
 \.
 
 
@@ -1617,6 +1617,6 @@ SELECT pg_catalog.setval('"public"."workflow_statistics_id_seq"', 1, false);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict sKvEdDwCHAs7WxHSu8dc8ItCk5lf0FUjny0YceEzaK6zMg5QoyBxwewwGmBgndA
+-- \unrestrict FDhtaOhtkA6bhhGBnzBSdCQs0cAhOfbg2vUarX7z2ytqNEIZ1rEd0tbH9HzYTeL
 
 RESET ALL;
